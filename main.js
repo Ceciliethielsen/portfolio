2bcb49ee425f69e71f3f1620745ce7d3cfe6ffd1
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-08-03 15:25:41 +0200

    Layout

---
+++ main.js
@@ -0,0 +1,11 @@
+  //alert("virker det?")
+  //variablerne er defineret herunder - hentet fra DOM
+  let burgerMenu = document.getElementById('burgermenu');
+  let overlay = document.getElementById('navbar');
+
+  //clickfunktion på burgermenuen er aktiveret via et event og en funktion starter
+  burgerMenu.addEventListener('click',function(){
+    //toggle er en skifte-funktion, som bruges til at skjule og vise de valgte elementer - her både "close" og "overlay"
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+  });
