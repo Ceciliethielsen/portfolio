e93cc3ea59cf1039a2508480a3e7422e4c916591
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-08-10 16:19:23 +0200

    scroll-knappen virker!

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-//Cecilie JS start
-  
+  //alert("virker det?")
   //variablerne er defineret herunder - hentet fra DOM
   let burgerMenu = document.getElementById('burgermenu');
-  let overlay = document.getElementById('menu');
+  let overlay = document.getElementById('navbar');
 
   //clickfunktion på burgermenuen er aktiveret via et event og en funktion starter
   burgerMenu.addEventListener('click',function(){
@@ -10,4 +9,23 @@
     this.classList.toggle("close");
     overlay.classList.toggle("overlay");
   });
-//Cecilie JS slut
+
+  //Get the button:
+mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
